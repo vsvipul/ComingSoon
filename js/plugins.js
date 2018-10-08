d4e419feef314bf45b270ee3b293d966a26d5ad1
vsvipul <vsvipul555@gmail.com>
2018-10-08 18:44:10 +0530

    initial commit

---
+++ js/plugins.js
@@ -0,0 +1,40 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+
+// -------------------------------------------------------------
+// Countdown Time Setup
+// -------------------------------------------------------------
+
+$('.countdown').downCount({
+    date: '09/30/2019 12:00:00',
+    offset: +10
+}, function () {});
+     
+/**
+ * MAILCHIMP
+ */
+$('#mc-form').ajaxChimp({ 
+    url: 'http://doelit.us10.list-manage.com/subscribe/post?u=b02e4f21e264536eff4820875&amp;id=4d57faf2cb' 
+    /* Replace Your AjaxChimp Subscription Link */
+});
